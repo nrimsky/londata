9cf5a04b6e3aae56ca653fb2f8a039d6783fcb92
nrimsky <ninarimsky@gmail.com>
2021-02-26 16:52:04 +0000

    Initial Commit

---
+++ londata/londataApp.swift
@@ -0,0 +1,17 @@
+//
+//  londataApp.swift
+//  londata
+//
+//  Created by Nina Rimsky on 26/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct londataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
